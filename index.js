8b999d705e7f6b40d416733b3cbce3493c4c5b83
Samuel <samuel.kodehode@gmail.com>
2024-01-04 15:33:50 +0100

    01.01

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 
 const sectionContainer = document.getElementById("section-1")
 
-for (let gradientCount = 1; gradientCount <= 15; gradientCount++) {
+for (let gradientCount = 0; gradientCount <= 60; gradientCount++) {
     const gradient = document.createElement("div")
     gradient.classList.add("gradient-box")
     gradient.id = `box-${gradientCount}`
-    gradient.style.left = `${gradientCount*10}px`;
-    gradient.style.top = `${gradientCount*20}px`;
+
+    gradient.style.right = `${gradientCount * 2 - 10}%`;
+    gradient.style.top = `${gradientCount * 2 - 10}%`;
+    gradient.style.scale = `${gradientCount * 0.06 + 0.4}`
+
+    gradient.addEventListener("mouseover", () => {
+        gradient.style.top = `${gradientCount * 2 }%`
+    })
+
+    gradient.addEventListener("mouseout", () => {
+        gradient.style.right = `${gradientCount * 2 - 10}%`;
+        gradient.style.top = `${gradientCount * 2 - 10}%`;
+        gradient.style.scale = `${gradientCount * 0.06 + 0.4}`
+    })
+
     sectionContainer.appendChild(gradient)
 }
+
+
+
