923f3548c87f5181d1bd41480f5935387abc9737
Samuel <samuel.kodehode@gmail.com>
2024-01-04 16:08:39 +0100

    01.02

--- index.js
+++ index.js
@@ -2,27 +2,31 @@
 
 const sectionContainer = document.getElementById("section-1")
 
-for (let gradientCount = 0; gradientCount <= 60; gradientCount++) {
+for (let gradientCount = 0; gradientCount <= 44; gradientCount++) {
     const gradient = document.createElement("div")
     gradient.classList.add("gradient-box")
     gradient.id = `box-${gradientCount}`
 
-    gradient.style.right = `${gradientCount * 2 - 10}%`;
-    gradient.style.top = `${gradientCount * 2 - 10}%`;
-    gradient.style.scale = `${gradientCount * 0.06 + 0.4}`
+    gradient.style.left = `${gradientCount * 3 - 10}%`;
+    gradient.style.bottom = `${25 + (Math.cos(gradientCount * 0.19) * gradientCount)}%`;
+    gradient.style.scale = `${1.3}`
+    gradient.style.rotate = `${gradientCount * 4 + 180 }deg`
+
 
     gradient.addEventListener("mouseover", () => {
-        gradient.style.top = `${gradientCount * 2 }%`
+        gradient.style.bottom = `${25 + (Math.cos(gradientCount * 0.2) * gradientCount) - 10}%`;
     })
 
     gradient.addEventListener("mouseout", () => {
-        gradient.style.right = `${gradientCount * 2 - 10}%`;
-        gradient.style.top = `${gradientCount * 2 - 10}%`;
-        gradient.style.scale = `${gradientCount * 0.06 + 0.4}`
+        gradient.style.bottom = `${25 + (Math.cos(gradientCount * 0.2) * gradientCount)}%`;
+
     })
+
 
     sectionContainer.appendChild(gradient)
 }
+
+
 
 
 
